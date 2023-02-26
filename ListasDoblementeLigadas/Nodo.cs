ef9a58354123287334d8e034f581de81022183fa
Juan Martinez <jfmg.64@gmail.com>
2023-02-25 19:08:09 -0700

    Se añadió la clase Lista y Nodo basado en Proyecto "ListasSimplementeLigadas" de AntonioRogelUK. Se añadió la propiedad tipo Nodo "Anterior". Se modificó listas y Nodo, para considerar los cambios en clase Nodo.

---
+++ ListasDoblementeLigadas/Nodo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDoblementeLigadas
+{
+    internal class Nodo
+    {
+        public Nodo Anterior { get; set; }
+        public String Valor { get; set; }
+        public Nodo Siguiente { get; set; }
+        public Nodo(String valor = "",Nodo anterior = null, Nodo siguiente = null)
+        {
+            Anterior = anterior;
+            Valor = valor;
+            Siguiente = siguiente;
+        }
+    }
+}
